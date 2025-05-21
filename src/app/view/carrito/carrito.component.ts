c4bc0ea4024f1d3de6114624b36acd36b973c170
Jesus <alonstarkj@gmail.com>
2025-05-20 19:34:29 -0500

    update

--- src/app/view/carrito/carrito.component.ts
+++ src/app/view/carrito/carrito.component.ts
@@ -6,6 +6,12 @@ import { FormControl, FormGroup, Validators,ReactiveFormsModule } from '@angular
 import { CardsCarritoComponent } from './cards-carrito/cards-carrito.component';
 import { pedido,GetDataBaseService } from 'src/app/get-data-base.service';
 
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ConfirmDialog } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
+
+
 
 
 
@@ -13,14 +19,15 @@ import { pedido,GetDataBaseService } from 'src/app/get-data-base.service';
   selector: 'app-carrito',
   templateUrl: './carrito.component.html',
   styleUrls: ['./carrito.component.css'],
+  imports: [CommonModule, CardsCarritoComponent,ReactiveFormsModule,ConfirmDialog,ToastModule,ButtonModule],
   standalone: true,
-  imports: [CommonModule, CardsCarritoComponent,ReactiveFormsModule]
+  providers: [ConfirmationService, MessageService],
 })
 export class CarritoComponent implements OnInit {
   visible:boolean = false;
   deleteText:string = "¿Está seguro de limpiar el carrito de compras, esta acción es irreversible?"
 
-  constructor(private carritoService: CarritoService,private dbService:GetDataBaseService) {}
+  constructor(private carritoService: CarritoService,private dbService:GetDataBaseService,private confirmationService: ConfirmationService, private messageService: MessageService) {}
 
   productosEnCarrito: Carrito[] = [];
 
@@ -35,9 +42,14 @@ export class CarritoComponent implements OnInit {
   }
   
   clearCart() {
-    if(confirm(this.deleteText)) this.carritoService.limpiarCarrito();
+    this.confirmComponent(this.deleteText,"Confirmación",()=>{
+      this.carritoService.limpiarCarrito()
+      return this.messageService.add({ severity: 'info', summary: 'Carrito Limpio', detail: 'Carrito limpiado con exito' });
+    },
+      this.procesoIncompleto)
     return
   }
+
 
   
   contactos:boolean=false
@@ -50,9 +62,14 @@ export class CarritoComponent implements OnInit {
     contacto: new FormControl( null,[Validators.required, Validators.min(900000000),Validators.max(999999999)])
   });
   
+  // CONFIRM DE PRIMENG
+  position: 'left' | 'right' | 'top' | 'bottom' | 'center' | 'topleft' | 'topright' | 'bottomleft' | 'bottomright' = 'top';
+  enviar(){
+    this.confirmComponent("¿Estás seguro de enviar?","Confirmación",this.enviarPedido,this.procesoIncompleto)
+  }
 
-  enviarPedido(){
-    if(this.miFormulario.invalid) return alert("Complete todos los campos requeridos.")
+  enviarPedido=()=>{
+    if(this.miFormulario.invalid) return this.messageService.add({ severity: 'warn', summary: 'Incompleto', detail: 'Complete todos los campos requeridos' });
 
     const dni=this.miFormulario.get('dni')?.value
     const contacto=this.miFormulario.get('contacto')?.value
@@ -74,10 +91,10 @@ export class CarritoComponent implements OnInit {
           this.viewFormContacts()
           this.visible=false
           
-          return alert("Pedido enviado con exito.")
+          return this.messageService.add({ severity: 'success', summary: 'Pedido Enviado', detail: 'Pedido enviado con exito' });
         },
         error: () => {
-          return alert("Pedido fallido.")
+          return this.messageService.add({ severity: 'error', summary: 'Pedido Fallido', detail: 'Pedido no enviado o rechazado' });
         },
         complete:()=>{
           
@@ -86,9 +103,32 @@ export class CarritoComponent implements OnInit {
       
       
     }else{
-      return alert("Invalido.")
+      return  this.messageService.add({ severity: 'warn', summary: 'Invalido', detail: 'Datos invalidos' });
     }
   }
+  procesoIncompleto=()=>{
+    return this.messageService.add({severity: 'error',summary: 'Incompleto',detail: 'Proceso incompleto',life: 3000,});
+  }
 
+  confirmComponent(message:string,header:string,accept:Function,reject:Function,){
+      this.confirmationService.confirm({
+              message,
+              header,
+              icon: 'pi pi-info-circle',
+              rejectButtonStyleClass: 'p-button-text',
+              rejectButtonProps: {
+                  label: 'Cancelar',
+                  severity: 'secondary',
+                  text: true,
+              },
+              acceptButtonProps: {
+                  label: 'Confirmar',
+                  text: true,
+              },
+              accept,
+              reject,
+              key: 'positionDialog',
+          });
+  }
   
 }
